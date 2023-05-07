142d0d0592316784da889df0ff146f2f388efee3
Babatunde Ojo <babatundeojo30@gmail.com>
2023-05-07 20:50:58 +0100

    remove unused declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react";
 import {
   BrowserRouter,
-  Outlet,
   Route,
   Routes
 } from "react-router-dom";
@@ -11,25 +9,8 @@ import Landing from "./features/landing/ui";
 import Test from "./features/test/ui";
 import Result from "./features/result/ui";
 import NotFound from "./features/notfound/ui";
-import './App.css';
 
 export function App(): JSX.Element {
-  useEffect(() => {
-    switch (localStorage.theme) {
-      case "dark":
-        document.documentElement.classList.add("dark");
-        break;
-      case "light":
-        document.documentElement.classList.remove("dark");
-        break;
-      default:
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches)
-          document.documentElement.classList.add("dark");
-        else document.documentElement.classList.remove("dark");
-        break;
-    }
-  }, []);
-
   return (
     <Background className="h-auto w-auto">
       <Navigation>
