5dd489c17f143bceb487bc2905902a66a9ee8046
Babatunde Ojo <babatundeojo30@gmail.com>
2023-05-12 00:21:26 +0100

    add unit test for test page

--- src/features/test/ui/__test__/testPAge.test.tsx
+++ src/features/test/ui/__test__/testPAge.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen, within } from "@testing-library/react";
 import { createRenderer } from "react-test-renderer/shallow";
+import { act } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
 import Test from "..";
 
@@ -15,18 +16,24 @@ jest.mock('react-router-dom', () => ({
 
 describe("Unit test for test page ", () => {
 
-    it("should render and match the snapshot", () => {
-        renderer.render(defaultComponent);
+    it("should render and match the snapshot", async () => {
+        //renderer.render(defaultComponent);
+        await act( async () => renderer.render(defaultComponent));
+
         const renderedOutput = renderer.getRenderOutput();
 
         expect(renderedOutput).toMatchSnapshot();
     });
 
-    it("should have title as Your result", () => {
-        const { getByRole } = render(defaultComponent);
-        const titleHeading = getByRole("heading", { name: "Are you an introvert or an extrovert?" });
+    it("should have title as Are you an introvert or an extrovert?", async () => {
+        await act( async () => {
+            const { getByRole } = render(defaultComponent);
+            const titleHeading = getByRole("heading", { name: "Are you an introvert or an extrovert?", hidden: true });
 
-        expect(titleHeading).toBeVisible();
+            console.log(window.location.href)
+
+            expect(titleHeading).toBeVisible();
+        });
     });
 
 });
